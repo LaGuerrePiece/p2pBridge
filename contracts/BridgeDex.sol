d60030b0195ce854a20e8510a2dc7a0cb7a7cc2d
turboblitz <florent.tavernier@gmail.com>
2022-07-23 17:26:23 +0200

    initial commit

--- contracts/BridgeDex.sol
+++ contracts/BridgeDex.sol
@@ -5,25 +5,20 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 struct ChainAChallenge {
-    uint256 bidAmount;
     uint256 otherChain;
     uint256 nonce;
     bytes challengerSignature;
     address challenger;
     address token;
-    bool accepted;
 }
 
 struct ChainBChallenge {
-    uint256 bidAmount;
     uint24 fees;
     bytes bridgerSignature;
 }
 
 struct Lock {
     uint256 amount;
-    uint256 bridged;
-    uint256 accepted;
     ChainAChallenge[] challenges;
     address token;
     address owner;
@@ -34,7 +29,6 @@ struct Lock {
 
 struct Request {
     uint256 amount;
-    uint256 minBidAmount;
     uint256 chainAId;
     uint256 chainANonce;
     uint256 index;
@@ -75,7 +69,6 @@ contract BridgeDex {
         uint256 indexed id,
         address indexed bridger,
         uint256 amount,
-        uint256 _minBidAmount,
         uint256 _chainAId,
         uint256 _chainANonce,
         bytes _signature,
@@ -133,8 +126,6 @@ contract BridgeDex {
         if (l.challenged) require(l.date + 7 days < block.timestamp);
         require(l.owner == msg.sender);
 
-        uint256 amount = l.amount - l.bridged;
-        address token = l.token;
         delete idToLock[_lockId];
 
         for (uint256 i = 0; i < myLocks[msg.sender].length; ++i) {
@@ -144,14 +135,13 @@ contract BridgeDex {
             }
         }
 
-        ERC20(token).transfer(msg.sender, amount);
+        ERC20(l.token).transfer(msg.sender, l.amount);
     }
 
     /**
      * @notice Used to publish a bridge request on chainB
      *
      * @param _amount - The total amount of token to bridge
-     * @param _minBidAmount - The minimal amount the bridge seeker is willing to accept for an operation
      * @param _date - The date corresponding to the chain A lock
      * @param _chainAId - The id of the chain where the bridger will provide tokens
      * @param _chainANonce - The nonce of the lock on the chain A
@@ -163,7 +153,6 @@ contract BridgeDex {
      */
     function publishRequest(
         uint256 _amount,
-        uint256 _minBidAmount,
         uint256 _date,
         uint256 _chainAId,
         uint256 _chainANonce,
@@ -184,7 +173,6 @@ contract BridgeDex {
         tokenContractToRequestId[_tokenBContract].push(_nonce);
         Request storage request = idToRequest[_nonce];
         request.amount = _amount;
-        request.minBidAmount = _minBidAmount;
         request.chainAId = _chainAId;
         request.chainANonce = _chainANonce;
         request.initialSignature = _signature;
@@ -207,10 +195,37 @@ contract BridgeDex {
     }
 
     /**
-     * @notice Used to become the challenger of a given lock on the chainA
-     * The user first submits its challenge and needs to be accepted
+     * @notice Used to become one of the challengers on the chain B and
+     * to deposit the tokens for becoming a challenger
      *
-     * @param _bidAmount - The amount the provider is willing to provide to the bridger from chain B
+     * @param _id - the id for the mapping of the request to become challenger of
+     * @param _fees - The amount of fees the challenger want to keep
+     */
+    function becomeChainBChallenger(uint256 _id, uint256 _fees) external {
+        Request storage request = idToRequest[_id];
+        require(
+            request.challenges[msg.sender].fees == 0,
+            "You already locked funds for this request"
+        );
+        require(request.amount != 0, "The request has already been fulfilled");
+        bytes memory none;
+        request.challenges[msg.sender] = ChainBChallenge({
+            fees: uint24(_fees),
+            bridgerSignature: none
+        });
+        uint256 amount = request.amount - (_fees * request.amount) / 2e6;
+        myDeposits[msg.sender].push(_id);
+
+        ERC20(request.tokenBContract).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+    }
+
+    /**
+     * @notice Used to become the challenger of a given lock on the chainA
+     *
      * @param _chainBId - The id of the chainB
      * @param _nonce - The nonce of the transaction
      * @param _initialSignature - The signature provided by the the bridger on chain B (to be verified here)
@@ -218,93 +233,20 @@ contract BridgeDex {
      *
      */
     function becomeChainAChallenger(
-        uint256 _bidAmount,
         uint256 _chainBId,
         uint256 _nonce,
         bytes memory _initialSignature,
+        bytes memory _challengerSignature,
         uint256 _lockId
     ) external {
         Lock storage l = idToLock[_lockId];
         require(l.locked, "The bridge seeker is not searching to bridge");
         require(l.date + 5 days > block.timestamp, "The lock is out of date");
-        require(
-            l.amount >= _bidAmount,
-            "The bidAmount is greated than the requested amount"
-        );
 
         _verify(
             l.owner,
             _initialSignature,
             abi.encodePacked(_nonce, _chainBId, _lockId, chainId)
-        );
-
-        bytes memory none;
-        l.challenges.push(
-            ChainAChallenge({
-                token: l.token,
-                bidAmount: _bidAmount,
-                otherChain: _chainBId,
-                nonce: _nonce,
-                challenger: msg.sender,
-                challengerSignature: none,
-                accepted: false
-            })
-        );
-        myChallenges[msg.sender].push(_lockId);
-        l.challenged = true;
-    }
-
-    /**
-     * @notice - Used by the bridger to choose the challengers he wants to went with
-     *
-     * @param _lockId - The lock for the given transaction
-     * @param _challengerId - The id of the challenger into the challengers to accept the challenge
-     *
-     */
-    function acceptChainAChallenger(uint256 _lockId, uint256 _challengerId)
-        external
-    {
-        Lock storage l = idToLock[_lockId];
-
-        require(l.locked, "The bridge seeker is not searching to bridge");
-        require(l.date + 5 days > block.timestamp, "The lock is out of date");
-        require(l.owner == msg.sender, "You are not the owner of the lock");
-        require(
-            l.amount >= l.accepted + l.challenges[_challengerId].bidAmount,
-            "Cannot accept more than the initial amount"
-        );
-
-        l.challenges[_challengerId].accepted = true;
-        l.accepted += l.challenges[_challengerId].bidAmount;
-    }
-
-    /**
-     * @notice - Used to submit the signature after being accepted for a challenge
-     *
-     * @param _challengerId - The id of our submit on the lock challenges array
-     * @param _challengerSignature - The signature for the bridger to take our funds on the chain B
-     * @param _lockId - The id of the current lock
-     * @param _initialSignature - The signature of the bridger for the given transaction on chain B
-     */
-    function submitChainAChallengerSignature(
-        uint256 _challengerId,
-        bytes memory _challengerSignature,
-        uint256 _lockId,
-        bytes memory _initialSignature
-    ) external {
-        Lock storage l = idToLock[_lockId];
-        require(l.date + 5 days > block.timestamp, "The lock is out of date");
-        require(
-            l.challenges[_challengerId].challenger == msg.sender,
-            "You are not the challenger you pretend to be"
-        );
-        require(
-            l.challenges[_challengerId].accepted,
-            "Not accepted by the briger"
-        );
-        require(
-            l.challenges[_challengerId].challengerSignature.length == 0,
-            "The request has already been signed"
         );
 
         _verify(
@@ -313,42 +255,17 @@ contract BridgeDex {
             abi.encodePacked(_initialSignature)
         );
 
-        l.challenges[_challengerId].challengerSignature = _challengerSignature;
-    }
-
-    /**
-     * @notice Used to become one of the challengers on the chain B and
-     * to deposit the tokens for becoming a challenger
-     *
-     * @param _id - the id for the mapping of the request to become challenger of
-     * @param _bidAmount - The amount to be deposited to the bridge
-     * @param _fees - The amount of fees the challenger want to keep
-     */
-    function becomeChainBChallenger(
-        uint256 _id,
-        uint256 _bidAmount,
-        uint256 _fees
-    ) external {
-        Request storage request = idToRequest[_id];
-        require(
-            request.challenges[msg.sender].bidAmount == 0,
-            "You already locked funds for this request"
+        l.challenges.push(
+            ChainAChallenge({
+                token: l.token,
+                otherChain: _chainBId,
+                nonce: _nonce,
+                challenger: msg.sender,
+                challengerSignature: _challengerSignature
+            })
         );
-        require(request.amount != 0, "The request has already been fulfilled");
-        bytes memory none;
-        request.challenges[msg.sender] = ChainBChallenge({
-            fees: uint24(_fees),
-            bidAmount: _bidAmount,
-            bridgerSignature: none
-        });
-        uint256 amount = _bidAmount - (_fees * _bidAmount) / 2e6;
-        myDeposits[msg.sender].push(_id);
-
-        ERC20(request.tokenBContract).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
+        myChallenges[msg.sender].push(_lockId);
+        l.challenged = true;
     }
 
     /**
@@ -361,11 +278,10 @@ contract BridgeDex {
         ];
         Request storage request = idToRequest[_id];
 
-        require(challenge.bidAmount != 0);
         require(request.date + 5 days < block.timestamp || request.amount == 0);
 
-        uint256 amount = challenge.bidAmount -
-            (challenge.fees * challenge.bidAmount) /
+        uint256 amount = request.amount -
+            (challenge.fees * request.amount) /
             2e6;
         delete idToRequest[_id].challenges[msg.sender];
         ERC20(request.tokenBContract).transfer(msg.sender, amount);
@@ -387,14 +303,11 @@ contract BridgeDex {
     ) external {
         Request storage request = idToRequest[_id];
 
-        uint256 amount = request.challenges[_challenger].bidAmount;
-        request.amount -= amount;
-
-        uint256 fees = (amount * request.challenges[_challenger].fees) / 1e6;
-        amount = amount - fees;
+        uint256 fees = (request.amount * request.challenges[_challenger].fees) /
+            1e6;
+        uint256 amount = request.amount - fees;
 
         require(request.date + 5 days > block.timestamp);
-        require(request.challenges[_challenger].bidAmount != 0);
 
         _verify(
             _challenger,
@@ -409,21 +322,19 @@ contract BridgeDex {
         );
 
         request.challenges[_challenger].bridgerSignature = _bridgerSignature;
-        request.challenges[_challenger].bidAmount = 0;
+        request.amount = 0;
 
-        if (request.amount == 0) {
-            _splice(
-                tokenContractToRequestId[request.tokenBContract],
-                request.index
-            );
-        }
+        _splice(
+            tokenContractToRequestId[request.tokenBContract],
+            request.index
+        );
 
         ERC20(request.tokenBContract).transfer(msg.sender, amount);
         ERC20(request.tokenBContract).transfer(owner, fees / 2);
     }
 
     /**
-     * @notice Used by the provided to withdraw its funds on the chain A
+     * @notice Used by the provider to withdraw his funds on chain A
      *
      * @param _bridgerSignature - The last signature of the bridger allonwing the provider to withdraw its funds on chain A
      * @param _challengeId - The id of the challenge in the challenge list
@@ -436,18 +347,16 @@ contract BridgeDex {
     ) external {
         Lock storage l = idToLock[_lockId];
         ChainAChallenge storage challenge = l.challenges[_challengeId];
+
         _verify(
             l.owner,
             _bridgerSignature,
             abi.encodePacked(challenge.challengerSignature)
         );
 
-        l.bridged += challenge.bidAmount;
-        if (l.amount == l.bridged) {
-            delete idToLock[_lockId];
-        }
+        delete idToLock[_lockId];
 
-        ERC20(l.token).transfer(challenge.challenger, challenge.bidAmount);
+        ERC20(l.token).transfer(challenge.challenger, l.amount);
     }
 
     /**
