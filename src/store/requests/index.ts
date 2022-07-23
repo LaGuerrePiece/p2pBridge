4ee4c1a6fe9f749b035affa980ec1956eeccf32b
sylar123abc <sylar123abc@gmail.com>
2022-07-23 17:58:30 +0200

    Adding getter for request challenge

--- src/store/requests/index.ts
+++ src/store/requests/index.ts
@@ -1,23 +1,33 @@
 import { defineStore } from "pinia";
 import { BridgeDexInstance } from "../../../types/truffle-contracts";
-import { RequestActions, RequestGetters, RequestState } from "../../types/requests";
-import { myRequests } from "./getters";
+import {
+  RequestActions,
+  RequestGetters,
+  RequestState,
+} from "../../types/requests";
+import { addLock, addRequest } from "./actions";
+import { getRequestChallenges, myRequests } from "./getters";
 import { state } from "./state";
 
 export const useRequestStore = defineStore("requests", {
   state: (): RequestState => state,
   getters: {
-    [RequestGetters.MyRequests]: myRequests
+    [RequestGetters.MyRequests]: myRequests,
+    [RequestGetters.RequestChallenges]: getRequestChallenges,
   },
   actions: {
     [RequestActions.AddRequest](
       request: Awaited<ReturnType<BridgeDexInstance["idToRequest"]>>,
       chainId: number
-    ): void {},
+    ): void {
+      return addRequest.bind(this)(request, chainId);
+    },
     [RequestActions.AddLock](
       lock: Awaited<ReturnType<BridgeDexInstance["idToLock"]>>,
       chainId: number,
       lockNonce: number
-    ): void {},
+    ): void {
+      return addLock.bind(this)(lock, chainId, lockNonce);
+    },
   },
 });
--- src/types/requests.ts
+++ src/types/requests.ts
@@ -39,5 +39,6 @@ export const RequestActions = {
 } as const  
 
 export const RequestGetters = {
-  MyRequests: "MyRequests"
+  MyRequests: "MyRequests",
+  RequestChallenges: "RequestChallenges"
 }
