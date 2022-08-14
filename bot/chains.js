f1d15224a1f6ca9bfbfa4f2472023b2cd8961058
Stormower <nicolasletimon@gmail.com>
2022-08-14 17:32:10 +0200

    update contracts

--- bot/chains.js
+++ bot/chains.js
@@ -6,9 +6,11 @@ module.exports = [
         "https://rinkeby.infura.io/v3/49f373294ecd4358abd6a39d55521529",
         "wss://rinkeby.infura.io/ws/v3/49f373294ecd4358abd6a39d55521529",
       ],
-      bridgeAddress: "0x12e567f179c4fAa810f235fc73C9874B356c829E",
+      bridgeAddress: "0xe805Ee75a511427F56A1F851859aF98290608316",
       token: {
         WETH: { address: "0xc778417E063141139Fce010982780140Aa0cD5Ab" },
+        NUKE: { address: "0x35C5d2EDef97edcb682eCa2D168F325c8AE1fD22" },
+
       },
     },
     {
@@ -18,9 +20,10 @@ module.exports = [
         "https://kovan.infura.io/v3/49f373294ecd4358abd6a39d55521529",
         "wss://kovan.infura.io/ws/v3/49f373294ecd4358abd6a39d55521529",
       ],
-      bridgeAddress: "0x47bfCBfBFE9654248D5B55E85fAF9bFE00194E60",
+      bridgeAddress: "0x1ea0da6c3d97a6f69d5a0ebc6c618fd146bef44a",
       token: {
         WETH: { address: "0xd0A1E359811322d97991E03f863a0C30C2cF029C" },
+        NUKE: { address: "0x8900B2DA7aE751CA6780d9C0B09B165eBC9d80F4" },
 
       },
   }
--- src/composition/constants.ts
+++ src/composition/constants.ts
@@ -40,7 +40,7 @@ export const chainDetails: ChainDetails = {
       "https://rinkeby.infura.io/v3/49f373294ecd4358abd6a39d55521529",
       "wss://rinkeby.infura.io/ws/v3/49f373294ecd4358abd6a39d55521529",
     ],
-    bridgeAddress: "0x12e567f179c4fAa810f235fc73C9874B356c829E",
+    bridgeAddress: "0xe805Ee75a511427F56A1F851859aF98290608316",
     token: {
       WETH: { name: "WETH", icon: weth, address: "0xc778417E063141139Fce010982780140Aa0cD5Ab" },
       NUKE: { name: "NUKE", icon: weth,address: "0x35C5d2EDef97edcb682eCa2D168F325c8AE1fD22" },
@@ -57,7 +57,7 @@ export const chainDetails: ChainDetails = {
       "https://kovan.infura.io/v3/49f373294ecd4358abd6a39d55521529",
       "wss://kovan.infura.io/ws/v3/49f373294ecd4358abd6a39d55521529",
     ],
-    bridgeAddress: "0x47bfCBfBFE9654248D5B55E85fAF9bFE00194E60",
+    bridgeAddress: "0x1ea0da6c3d97a6f69d5a0ebc6c618fd146bef44a",
     token: {
       WETH: { name: "WETH", icon: weth, address: "0xd0A1E359811322d97991E03f863a0C30C2cF029C" },
       NUKE: { name: "NUKE", icon: weth, address: "0x8900B2DA7aE751CA6780d9C0B09B165eBC9d80F4" },
